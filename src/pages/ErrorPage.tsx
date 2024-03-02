fc88ce24161695e192264ab8d9b16341c2b62e80
Artem Glebov <artem.glebov2004@yandex.ru>
2024-03-02 17:15:31 +0300

    убрал ошибки

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,12 +1,11 @@
-import { Link, useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  const error = useRouteError();
   return (
     <div className="relative flex gap-20 items-center justify-center flex-col py-5 overflow-hidden h-screen">
       <img src="/bg-error.png" alt="" className="absolute -z-10 left-[15%]" />
       <h1 className="uppercase font-black text-3xl text-center">
-        <i>{error.statusText || error.message}</i>
+        <i>Not found</i>
       </h1>
       <img src="/error.svg" alt="" />
       <Link to="/" className="py-2 px-8 border-black border-4 flex items-center justify-center rounded-[30px] bg-[#E8F5FF]">
